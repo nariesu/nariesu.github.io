4bd16348290bb4d1af617f71cb73752de26b29f9
meredith <88279821+nariesu@users.noreply.github.com>
2023-05-20 15:43:37 -0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,24 @@
+// hamburger
+function hamburgerMenu() {
+    var x = document.getElementById("hamburgerToggle");
+    // var y = document.getElementById("hamburger")
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
+// menu list  
+$(function() {
+    $('.menu-toggle').on('click', function() {
+
+         // switch the class on the previously active div to make it hidden
+         $('.active-menu').removeClass('active-menu').addClass('inactive-menu');
+
+         // switch the class on the new active div to show it
+         var selectorForActiveMenu = $(this).attr('href');
+         $(selectorForActiveMenu).removeClass('inactive-menu').addClass('active-menu');
+    });
+});
+  
