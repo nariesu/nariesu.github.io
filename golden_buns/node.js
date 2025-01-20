e94fadcb683c3910f19747c25e3160dc65f55dc6
Meredith <88279821+nariesu@users.noreply.github.com>
2025-01-19 19:37:57 -0700

    Add files via upload

---
+++ golden_buns/node.js
@@ -0,0 +1,34 @@
+// hamburger
+function hamburgerMenu() {
+    var x = document.getElementById("hamburgerToggle");
+    // var y = document.getElementById("hamburger")
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+
+// hamburger animation
+$(document).ready(function(){
+	$('#hamburger').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+// menu list  
+$(function() {
+    $('.menu-toggle').on('click', function() {
+
+      window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      }
+
+         // switch the class on the previously active div to make it hidden
+         $('.active-menu').removeClass('active-menu').addClass('inactive-menu');
+
+         // switch the class on the new active div to show it
+         var selectorForActiveMenu = $(this).attr('href');
+         $(selectorForActiveMenu).removeClass('inactive-menu').addClass('active-menu');
+    });
+});
